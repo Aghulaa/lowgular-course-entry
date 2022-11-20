0c6fcfe730220c4c990675f59e6792a02f0a6a33
agnie <agnieszkaryzner2@gmail.com>
2022-11-20 20:59:18 +0100

    5. Feat: Exercise 1

--- src/app/ui/employee-list/employee-list.component.ts
+++ src/app/ui/employee-list/employee-list.component.ts
@@ -1,11 +1,7 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  ViewEncapsulation,
-} from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { EmployeeModel } from '../../model/employee.model';
-import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'employee-list',
@@ -14,6 +10,6 @@ import {HttpClient} from "@angular/common/http";
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EmployeeListComponent {
-  constructor(private _client: HttpClient) {}
-  data$: Observable<EmployeeModel[] | null> = of(null);
+  constructor(private _httpClient: HttpClient) {}
+  data$: Observable<EmployeeModel[] | null> = this._httpClient.get<EmployeeModel[]>('assets/data/employees.json');
 }
