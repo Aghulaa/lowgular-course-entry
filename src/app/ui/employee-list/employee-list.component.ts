d5744b2d30bf53877bcac1744d75bf1dee765d1b
agnie <agnieszkaryzner2@gmail.com>
2022-11-21 21:36:19 +0100

    7. Feat: Added pictures, changed them to horizontal view and removed/ commented unnecessary commends in others files.

--- src/app/ui/employee-list/employee-list.component.ts
+++ src/app/ui/employee-list/employee-list.component.ts
@@ -5,7 +5,8 @@ import { EmployeeModel } from '../../model/employee.model';
 
 @Component({
   selector: 'employee-list',
-  templateUrl: './employee-list.component.html',
+ templateUrl: './employee-list.component.html',
+  //template: '<h1>Inaczej</h1>',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 
@@ -13,5 +14,6 @@ import { EmployeeModel } from '../../model/employee.model';
 export class EmployeeListComponent {
   constructor(private _httpClient: HttpClient) {}
   data$: Observable<EmployeeModel[] | null> = this._httpClient.get<EmployeeModel[]>('assets/data/employees.json');
-  title: string='Tekst, który pojawi się w HTML';
+ // data = [{name: 'Agnieszka'},{name: 'Monika'}];
+  //title: string='Tekst, który pojawi się w HTML';
 }
