c12aacfa32ad9a2ef1ef4c136a39415a74220996
agnie <agnieszkaryzner2@gmail.com>
2022-11-26 17:24:52 +0100

    10. Feat: add ServiceModule by lowgular and change in employee-list.component.ts constructor from HttpClient to Service

--- src/app/ui/employee-list/employee-list.component.ts
+++ src/app/ui/employee-list/employee-list.component.ts
@@ -1,19 +1,21 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+//import { HttpClient } from '@angular/common/http'; <- this library is useless after change constructor
 import { Observable } from 'rxjs';
+import { EmployeeService } from '../../services/employee.service';
 import { EmployeeModel } from '../../model/employee.model';
 
 @Component({
   selector: 'employee-list',
- templateUrl: './employee-list.component.html',
+  templateUrl: './employee-list.component.html',
   //template: '<h1>Inaczej</h1>',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 
 })
 export class EmployeeListComponent {
-  constructor(private _httpClient: HttpClient) {}
-  data$: Observable<EmployeeModel[] | null> = this._httpClient.get<EmployeeModel[]>('assets/data/employees.json');
- // data = [{name: 'Agnieszka'},{name: 'Monika'}];
+  constructor(private _employeeService: EmployeeService) { }
+  data$: Observable<EmployeeModel[] | null> = this._employeeService.getAll();
+
+  // data = [{name: 'Agnieszka'},{name: 'Monika'}];
   //title: string='Tekst, który pojawi się w HTML';
 }
