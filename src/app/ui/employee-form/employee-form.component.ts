f9afc87a696e7f5d2da7fd1a40cb8633f8ed6969
agnie <agnieszkaryzner2@gmail.com>
2022-11-27 13:24:12 +0100

    17. Feat: Lesson 3, Exercise 3. Create employee from form. But alert doesn't work but it was optional task.

--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { EmployeeService } from '../../services/employee.service';
+import { CreateEmployeeModel } from '../../model/create-employee.model';
 
 @Component({
   selector: 'app-employee-form',
@@ -9,13 +11,22 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class EmployeeFormComponent {
   readonly employeeForm: FormGroup = new FormGroup({
-    email: new FormControl(null,[Validators.required]),
-    age: new FormControl(null,[Validators.min(0),Validators.required]),
-    salary: new FormControl(null,[Validators.min(0),Validators.required])
+    email: new FormControl(null, [Validators.required]),
+    age: new FormControl(null, [Validators.min(0), Validators.required]),
+    salary: new FormControl(null, [Validators.min(0), Validators.required])
   });
 
-  oneButtonClicked(form: {email: string, age: number, salary: number}){
-    alert('User was successfully added to the database. Email: ' + form.email +
-      ', Age:' + form.age + ', Salary: ' + form.salary + "$");
+  constructor(private _employeeService: EmployeeService) {
+  }
+
+  // oneButtonClicked(form: { email: string, age: number, salary: number }) {
+  //   alert('User was successfully added to the database. Email: ' + form.email +
+  //     ', Age:' + form.age + ', Salary: ' + form.salary + "$");
+  // }
+  onFormSubmitted(form: CreateEmployeeModel) {
+    this._employeeService.create({email: form.email, age: form.age, salary: form.salary})
+      .subscribe(alert => 'User was successfully added to the database. Email: ' + form.email +
+        ', Age:' + form.age + ', Salary: ' + form.salary + "$");
+
   }
 }
